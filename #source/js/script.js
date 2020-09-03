dbf42cff3d399ea5dbff102e323487262f0d1a44
ReginaYashina <reginayashina@inbox.ru>
2020-09-03 14:11:23 +0300

    add "promises" & "approach" sections

--- #source/js/script.js
+++ #source/js/script.js
@@ -287,3 +287,23 @@ function tip() {
 		$(this).toggleClass('active');
 	});
 }
+
+//SWITCH
+$(function () {
+	$('#switch-btn-1').click(function () {
+		$(this).toggleClass('switch-on');
+		if ($(this).hasClass('switch-on')) {
+			$(this).trigger('on.switch');
+		} else {
+			$(this).trigger('off.switch');
+		}
+	});
+	$('#switch-btn-1').on('on.switch', function () {
+		$('#block-1').addClass('bl-hide');
+		$('#block-2').removeClass('bl-hide');
+	});
+	$('.switch-btn').on('off.switch', function () {
+		$('#block-1').removeClass('bl-hide');
+		$('#block-2').addClass('bl-hide');
+	});
+});
